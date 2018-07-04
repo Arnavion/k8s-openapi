3b3e192ad2aa813bf48bc80c05517644a429abd6
Arnavion <arnavion@gmail.com>
2018-07-04 00:51:35 -0700

    CI now builds targets even if they can't be tested.

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,19 +1,25 @@
 set -euo pipefail
 
+: "${K8S_VERSION:=}"
+
 (
 	cd k8s-openapi-tests
 	cargo test --verbose --no-run --features "$FEATURE"
 ) &
 
-(
-	curl -Lo ~/minikube 'https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64'
-	chmod +x ~/minikube
+if [[ -n "$K8S_VERSION" ]]; then
+	(
+		curl -Lo ~/minikube 'https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64'
+		chmod +x ~/minikube
 
-	sudo CHANGE_MINIKUBE_NONE_USER=true ~/minikube start --vm-driver=none "--kubernetes-version=$K8S_VERSION"
-) &
+		sudo CHANGE_MINIKUBE_NONE_USER=true ~/minikube start --vm-driver=none "--kubernetes-version=$K8S_VERSION"
+	) &
+fi
 
 children="$(jobs -pr)"
 wait $children
 
-cd k8s-openapi-tests
-RUST_BACKTRACE=full cargo test --verbose --features "$FEATURE"
+if [[ -n "$K8S_VERSION" ]]; then
+	cd k8s-openapi-tests
+	RUST_BACKTRACE=full cargo test --verbose --features "$FEATURE"
+fi
