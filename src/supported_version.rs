b8b30b84a6df617cc255c66709c1719e08df000b
Arnavion <arnavion@gmail.com>
2018-12-16 13:40:53 -0800

    Updated v1.11 to v1.11.6

--- src/supported_version.rs
+++ src/supported_version.rs
@@ -38,7 +38,7 @@ impl SupportedVersion {
 			SupportedVersion::V1_8 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.8.15/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_9 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.9.11/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_10 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.10.11/api/openapi-spec/swagger.json",
-			SupportedVersion::V1_11 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.11.5/api/openapi-spec/swagger.json",
+			SupportedVersion::V1_11 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.11.6/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_12 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.12.3/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_13 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.13.1/api/openapi-spec/swagger.json",
 		}
