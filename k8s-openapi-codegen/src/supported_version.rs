93da22fc701723228b2444da828ff9200e31e63f
Arnavion <me@arnavion.dev>
2020-04-16 16:26:48 -0700

    Updated v1.16 to v1.16.9, v1,17 to v1,17.5, v1.18 to v1.18.2

--- k8s-openapi-codegen/src/supported_version.rs
+++ k8s-openapi-codegen/src/supported_version.rs
@@ -42,9 +42,9 @@ impl SupportedVersion {
 			SupportedVersion::V1_13 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.13.12/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_14 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.14.10/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_15 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.15.11/api/openapi-spec/swagger.json",
-			SupportedVersion::V1_16 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.16.8/api/openapi-spec/swagger.json",
-			SupportedVersion::V1_17 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.17.4/api/openapi-spec/swagger.json",
-			SupportedVersion::V1_18 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.18.1/api/openapi-spec/swagger.json",
+			SupportedVersion::V1_16 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.16.9/api/openapi-spec/swagger.json",
+			SupportedVersion::V1_17 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.17.5/api/openapi-spec/swagger.json",
+			SupportedVersion::V1_18 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.18.2/api/openapi-spec/swagger.json",
 		}
 	}
 
