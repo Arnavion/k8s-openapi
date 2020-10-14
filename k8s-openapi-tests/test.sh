86444b95f5d6828741d6eccce1ee9b844a64a74f
Arnavion <me@arnavion.dev>
2020-10-14 16:05:38 -0700

    Update v1.19 to v1.19.3

--- k8s-openapi-tests/test.sh
+++ k8s-openapi-tests/test.sh
@@ -49,7 +49,7 @@ declare -A K8S_VERSIONS=(
 	['1.16']='1.16.15'
 	['1.17']='1.17.12'
 	['1.18']='1.18.9'
-	['1.19']='1.19.2'
+	['1.19']='1.19.3'
 )
 
 # https://github.com/kubernetes-sigs/kind/releases
