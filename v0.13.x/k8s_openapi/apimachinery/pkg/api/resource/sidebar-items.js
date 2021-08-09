c34b8455131ee43b9239d03f107664824917f161
Arnavion <me@arnavion.dev>
2021-08-09 00:44:10 -0700

    8b9ee50d4c442c51509de886451a33f2ae7e5c17

---
+++ v0.13.x/k8s_openapi/apimachinery/pkg/api/resource/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Quantity","Quantity is a fixed-point representation of a number. It provides convenient marshaling/unmarshaling in JSON and YAML, in addition to String() and AsInt64() accessors."]]});
