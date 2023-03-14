7afb41d08e15252d698a714460d8f03e280486aa
Natalie Klestrup Röijezon <nat@nullable.se>
2023-03-14 18:19:50 +0100

    Run codegen

--- src/v1_22/list.rs
+++ src/v1_22/list.rs
@@ -31,9 +31,9 @@ impl<T> crate::Metadata for List<T> where T: crate::ListableResource {
     }
 }
 
-impl<T> crate::DeepMerge for List<T> where T: crate::ListableResource {
+impl<T> crate::DeepMerge for List<T> where T: crate::DeepMerge + crate::Metadata<Ty = crate::apimachinery::pkg::apis::meta::v1::ObjectMeta> + crate::ListableResource {
     fn merge_from(&mut self, other: Self) {
-        crate::DeepMerge::merge_from(&mut self.items, other.items);
+        crate::merge_strategies::list::map(&mut self.items, other.items, &[|lhs, rhs| lhs.metadata().namespace == rhs.metadata().namespace, |lhs, rhs| lhs.metadata().name == rhs.metadata().name], |inner_self, inner_other| {crate::DeepMerge::merge_from(inner_self, inner_other)});
         crate::DeepMerge::merge_from(&mut self.metadata, other.metadata);
     }
 }
