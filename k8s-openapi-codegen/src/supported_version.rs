70246bcf5e4b73b152f9257945060c3bbca66a25
Arnav Singh <me@arnavion.dev>
2023-06-14 13:54:45 -0700

    Update to v1.26.6

--- k8s-openapi-codegen/src/supported_version.rs
+++ k8s-openapi-codegen/src/supported_version.rs
@@ -56,7 +56,7 @@ impl SupportedVersion {
 			SupportedVersion::V1_23 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.23.17/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_24 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.24.15/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_25 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.25.11/api/openapi-spec/swagger.json",
-			SupportedVersion::V1_26 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.26.5/api/openapi-spec/swagger.json",
+			SupportedVersion::V1_26 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.26.6/api/openapi-spec/swagger.json",
 			SupportedVersion::V1_27 => "https://raw.githubusercontent.com/kubernetes/kubernetes/v1.27.2/api/openapi-spec/swagger.json",
 		}
 	}
