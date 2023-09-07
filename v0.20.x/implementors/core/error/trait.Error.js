864db4a6e0ba17088bac91e32f22186069511825
Arnav Singh <me@arnavion.dev>
2023-09-07 15:17:27 -0700

    52da26009c948cf02e82376212aee2ff63158590

---
+++ v0.20.x/implementors/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"k8s_openapi_codegen_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"k8s_openapi_codegen_common/struct.Error.html\" title=\"struct k8s_openapi_codegen_common::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
