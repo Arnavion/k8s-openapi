0f5af6f8e09e91962883f6a944894f0b0e5d59f5
Arnav Singh <me@arnavion.dev>
2024-03-14 13:42:33 -0700

    Fix devenv script for tmux 3.4

--- devenv.sh
+++ devenv.sh
@@ -11,13 +11,13 @@ if ! tmux has-session -t "=$session_name"; then
     tmux new-session -d -s "$session_name" -n '0'
     tmux select-pane -t "$session_name:0.0" -T 'git'
 
-    tmux split-window -t "$session_name:0.0" -h -p 50 "sleep 0.5 && echo 'clear; K8S_RECORD_=1 ./test.sh $max_version run-tests' && exec bash -l"
+    tmux split-window -t "$session_name:0.0" -h -l 50% "sleep 0.5 && echo 'clear; K8S_RECORD_=1 ./test.sh $max_version run-tests' && exec bash -l"
     tmux select-pane -t "$session_name:0.1" -T 'tests'
 
-    tmux split-window -t "$session_name:0.0" -v -p 50 $'cd ./k8s-openapi-codegen && sleep 0.5 && echo \'clear; cargo build && time bash -c "set -euo pipefail; cargo run --color always 2>&1 | ts" && cargo +stable clippy && echo "stable clippy OK" && cargo clippy\' && exec bash -l'
+    tmux split-window -t "$session_name:0.0" -v -l 50% $'cd ./k8s-openapi-codegen && sleep 0.5 && echo \'clear; cargo build && time bash -c "set -euo pipefail; cargo run --color always 2>&1 | ts" && cargo +stable clippy && echo "stable clippy OK" && cargo clippy\' && exec bash -l'
     tmux select-pane -t "$session_name:0.1" -T 'codegen'
 
-    tmux split-window -t "$session_name:0.2" -v -p 50 "sleep 0.5 && echo 'clear; ./ci/global.sh && K8S_OPENAPI_ENABLED_VERSION=$max_version ./ci/per_version.sh; echo \$?' && exec bash -l"
+    tmux split-window -t "$session_name:0.2" -v -l 50% "sleep 0.5 && echo 'clear; ./ci/global.sh && K8S_OPENAPI_ENABLED_VERSION=$max_version ./ci/per_version.sh; echo \$?' && exec bash -l"
     tmux select-pane -t "$session_name:0.3"
 fi
 
