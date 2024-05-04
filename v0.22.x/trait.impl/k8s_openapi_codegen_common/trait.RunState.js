5becb3ff98d0f4ce1be136094c8cc5617a8d586b
Arnav Singh <me@arnavion.dev>
2024-05-04 13:05:01 -0700

    ed70fb316da32b1d9155ad809a1ebbe0af98641b

---
+++ v0.22.x/trait.impl/k8s_openapi_codegen_common/trait.RunState.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"k8s_openapi_codegen_common":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
