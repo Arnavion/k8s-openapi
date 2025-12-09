30ea66212fbb32844c3271ed025195fe325b4db4
Jake Heinz <jheinz@x.ai>
2025-12-09 00:41:15 -0600

    fix

--- k8s-openapi-codegen-common/templates/quantity.rs
+++ k8s-openapi-codegen-common/templates/quantity.rs
@@ -10,31 +10,31 @@ impl<'de> {local}serde::Deserialize<'de> for {type_name} {{
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: {local}serde::Deserializer<'de> {{
         struct Visitor;
 
-        impl {local}serde::de::Visitor<'_> for Visitor {{
+        impl<'vde> {local}serde::de::Visitor<'vde> for Visitor {{
             type Value = {type_name};
 
             fn expecting(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {{
                 f.write_str("{type_name} (as an int or string)")
             }}
 
-            fn visit_newtype_struct<D>(self, deserializer: D) -> Result<Self::Value, D::Error> where D: {local}serde::Deserializer<'de> {{
+            fn visit_newtype_struct<D>(self, deserializer: D) -> Result<Self::Value, D::Error> where D: {local}serde::Deserializer<'vde> {{
                 deserializer.deserialize_string(Visitor)
             }}
 
             fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E> where E: {local}serde::de::Error {{
-                Ok({type_name}(v.to_string()))
+                Ok({type_name}(std::string::ToString::to_string(&v)))
             }}
 
             fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E> where E: {local}serde::de::Error {{
-                Ok({type_name}(v.to_string()))
+                Ok({type_name}(std::string::ToString::to_string(&v)))
             }}
 
             fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> where E: {local}serde::de::Error {{
-                Ok({type_name}(v.to_string()))
+                Ok({type_name}(std::string::ToString::to_string(&v)))
             }}
 
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: {local}serde::de::Error {{
-                self.visit_string(v.into())
+                self.visit_string(std::string::ToString::to_string(v))
             }}
 
             fn visit_string<E>(self, v: std::string::String) -> Result<Self::Value, E> where E: {local}serde::de::Error {{
